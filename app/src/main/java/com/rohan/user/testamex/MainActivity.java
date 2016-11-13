86093391907613f5068b52f8ca3462a416c150a7
rohanreddyt <rohantrr0830@gmail.com>
2016-11-13 15:50:26 -0500

    Initial Commit

---
+++ app/src/main/java/com/rohan/user/testamex/MainActivity.java
@@ -0,0 +1,52 @@
+package com.rohan.user.testamex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    int i=1;
+    TextView txt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txt = (TextView) findViewById(R.id.textView);
+        txt.setText(generate(1));
+    }
+    public void onIncrement(View view) {
+        i++;
+        txt.setText(generate(i));
+    }
+    public void onDecrement(View view) {
+        i--;
+        if(i>=1) {
+            txt.setText(generate(i));
+        }
+        else{
+            Toast.makeText(MainActivity.this,"Roman should be Greater than zero",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public static String generate(int num) {
+        StringBuilder sb = new StringBuilder();
+        int times = 0;
+        String[] romans = new String[] { "I", "IV", "V", "IX", "X", "XL", "L",
+                "XC", "C", "CD", "D", "CM", "M" };
+        int[] ints = new int[] { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500,
+                900, 1000 };
+        for (int i = ints.length - 1; i >= 0; i--) {
+            times = num / ints[i];
+            num %= ints[i];
+            while (times > 0) {
+                sb.append(romans[i]);
+                times--;
+            }
+        }
+        return sb.toString();
+    }
+
+
+}
